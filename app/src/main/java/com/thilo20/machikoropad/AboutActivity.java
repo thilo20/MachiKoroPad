1c39a348da073f36279bdd0e6fbd1d25f3070fe2
thilo20 <thilo.schaper@gmx.de>
2016-10-12 01:35:30 +0200

    adds dependency to lib MPAndroidChart, demo/tutorial for bar chart

--- app/src/main/java/com/thilo20/machikoropad/AboutActivity.java
+++ app/src/main/java/com/thilo20/machikoropad/AboutActivity.java
@@ -3,12 +3,87 @@ package com.thilo20.machikoropad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // enable for testing
+        if (false) {
+            demoBarChart();
+        }
+    }
+
+    /**
+     * Demonstrates showing a bar chart using lib MPAndroidChart.
+     * source url=https://www.numetriclabz.com/android-bar-chart-using-mpandroidchart-library-tutorial/
+     */
+    private void demoBarChart() {
+        // To make vertical bar chart, initialize graph id this way<br />
+        BarChart barChart = (BarChart) findViewById(R.id.chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        /* for create Grouped Bar chart
+        ArrayList<BarEntry> group1 = new ArrayList<>();
+        group1.add(new BarEntry(4f, 0));
+        group1.add(new BarEntry(8f, 1));
+        group1.add(new BarEntry(6f, 2));
+        group1.add(new BarEntry(12f, 3));
+        group1.add(new BarEntry(18f, 4));
+        group1.add(new BarEntry(9f, 5));
+
+        ArrayList<BarEntry> group2 = new ArrayList<>();
+        group2.add(new BarEntry(6f, 0));
+        group2.add(new BarEntry(7f, 1));
+        group2.add(new BarEntry(8f, 2));
+        group2.add(new BarEntry(12f, 3));
+        group2.add(new BarEntry(15f, 4));
+        group2.add(new BarEntry(10f, 5));
+
+        BarDataSet barDataSet1 = new BarDataSet(group1, "Group 1");
+        //barDataSet1.setColor(Color.rgb(0, 155, 0));
+        barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarDataSet barDataSet2 = new BarDataSet(group2, "Group 2");
+        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        ArrayList<BarDataSet> dataset = new ArrayList<>();
+        dataset.add(barDataSet1);
+        dataset.add(barDataSet2);
+        */
+
+        BarData data = new BarData(labels, dataset);
+
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        barChart.setData(data);
+        barChart.animateY(5000);
     }
 
 
